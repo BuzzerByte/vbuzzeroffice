c2222fdf66b5873a0086cdba137486d04eae093f
BuzzerByte <rujyi94@hotmail.com>
2020-06-24 23:33:53 +0800

    'merged'

--- resources/js/utils/request.js
+++ resources/js/utils/request.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import axios from 'axios';
-import { Message } from 'element-ui';
-import { getToken, setToken } from '@/utils/auth';
-
-// Create axios instance
-const service = axios.create({
-=======
 import '@/bootstrap';
 import { Message } from 'element-ui';
 // import { isLogged, setLogged } from '@/utils/auth';
 
 // Create axios instance
 const service = window.axios.create({
->>>>>>> 9d5fdcc792bc698dd171b1856cea47fe27fd55e8
   baseURL: process.env.MIX_BASE_API,
   timeout: 10000, // Request timeout
 });
@@ -20,18 +11,10 @@ const service = window.axios.create({
 // Request intercepter
 service.interceptors.request.use(
   config => {
-<<<<<<< HEAD
-    const token = getToken();
-    if (token) {
-      config.headers['Authorization'] = 'Bearer ' + getToken(); // Set JWT token
-    }
-
-=======
     // const token = isLogged();
     // if (token) {
     //   config.headers['Authorization'] = 'Bearer ' + isLogged(); // Set JWT token
     // }
->>>>>>> 9d5fdcc792bc698dd171b1856cea47fe27fd55e8
     return config;
   },
   error => {
@@ -44,17 +27,10 @@ service.interceptors.request.use(
 // response pre-processing
 service.interceptors.response.use(
   response => {
-<<<<<<< HEAD
-    if (response.headers.authorization) {
-      setToken(response.headers.authorization);
-      response.data.token = response.headers.authorization;
-    }
-=======
     // if (response.headers.authorization) {
     //   setLogged(response.headers.authorization);
     //   response.data.token = response.headers.authorization;
     // }
->>>>>>> 9d5fdcc792bc698dd171b1856cea47fe27fd55e8
 
     return response.data;
   },
@@ -72,11 +48,7 @@ service.interceptors.response.use(
       duration: 5 * 1000,
     });
     return Promise.reject(error);
-<<<<<<< HEAD
-  },
-=======
   }
->>>>>>> 9d5fdcc792bc698dd171b1856cea47fe27fd55e8
 );
 
 export default service;
